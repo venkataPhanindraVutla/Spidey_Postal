42db1d0c5ecffa597e1bc25e974b536f90a62798
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-09-13 23:56:34 +0530

    merge conflict 3

--- spidey/spidey/urls.py
+++ spidey/spidey/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from .settings import BASE_DIR
+from payment import urls as pays_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pay',include(pays_url),name= "payurls")
 ]
