a910080704f4f1161af00c4a74b2af0e1de1c715
saijayanth59 <sithlord5995@gmail.com>
2024-09-14 02:57:02 +0530

    resolve merge conflict

--- spidey/base/views.py
+++ spidey/base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Stamp
+from django.db.models import Q
+from .models import Stamp
 
 # Create your views here.
 
@@ -10,3 +12,9 @@ def home(request):
         'recent_stamps': recent_stamps,
         'popular_stamps': popular_stamps
     })
+
+def stamps(request):
+    q = request.GET.get('q', '')
+    stamps = Stamp.objects.filter(
+        Q(postal_circle__icontains=q) | Q(description__icontains=q) | Q(price__icontains=q) | Q(year__icontains=q)).order_by('-year')
+    return render(request, "stamps.html", {'stamps': stamps})
